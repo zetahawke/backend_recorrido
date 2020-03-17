13eddf27265a78013bf902723bfec06ba93cb451
zetahawke <michel.szinavel@gmail.com>
2020-03-17 17:43:37 -0300

    Adding seed information for populate basic information

--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -3,4 +3,5 @@ class Vehicle < ApplicationRecord
   has_one :company, through: :track_device
 
   validates :patent, presence: true, uniqueness: true
+  validates :track_device_id, presence: true, uniqueness: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,16 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'securerandom'
+
+default_company = Company.create(name: 'SomeDefault spa')
+default_kind_of_vehicles = %w[bus van truck sedan hatchback city_car].each { |vehicle_kind| VehicleKind.create(name: vehicle_kind) }
+default_vehicle_kind_to_populate = VehicleKind.find_by name: 'bus'
+
+19.times do |t|
+  track_device = TrackDevice.create!(company_id: default_company.id, serial: SecureRandom.hex)
+  Vehicle.create!(track_device_id: track_device.id, patent: SecureRandom.hex[0..3].upcase)
+end
+default_test_device = TrackDevice.create(company_id: default_company.id, serial: 'd131dd02c5e6eec4')
+default_test_vehicle = Vehicle.create(
+  track_device_id: default_test_device.id,
+  patent: SecureRandom.hex[0..3].upcase,
+  vehicle_kind_id: default_vehicle_kind_to_populate.id
+)
