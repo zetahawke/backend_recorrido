0b5d3f8e8884e65d0592ed360e8b0fcab3c8f4cd
zetahawke <michel.szinavel@gmail.com>
2020-03-29 23:33:30 -0300

    Feat: adding routes filter's endpoint
    - Added basic endpoint with needed class methods to filter
    
    Note: NON TDD applied!

--- app/models/road_route.rb
+++ app/models/road_route.rb
@@ -2,4 +2,26 @@ class RoadRoute < ApplicationRecord
   belongs_to :track_device
   belongs_to :vehicle
   has_many :road_route_points
+
+  class << self
+    def filter_by(params)
+      destiny_coords = params[:destiny] == 'psn' ? RoadRoutePoint.default_init : RoadRoutePoint.default_end
+      preload_data = params[:patent].blank? ? by_date(params[:date]) : by_date(params[:date]).by_vehicle_patent(params[:patent])
+      preload_data.when_road_to(destiny_coords)
+    end
+
+    def when_road_to(coords = RoadRoutePoint.default_init)
+      joins(:road_route_points).select do |rr|
+        rr.road_route_points.first.coords == coords
+      end
+    end
+
+    def by_vehicle_patent(patent)
+      joins(:vehicle).where(vehicles: { patent: patent })
+    end
+
+    def by_date(date)
+      where('created_at::date == ?', date.try(:to_date))
+    end
+  end
 end
--- app/models/road_route_point.rb
+++ app/models/road_route_point.rb
@@ -5,5 +5,24 @@ class RoadRoutePoint < ApplicationRecord
     def allowed_params
       %i[longitude latitude devise_serial_number]
     end
+
+    def points_by_coords(coords = default_init)
+      lon, lat = coords
+      where(latitude: lat, longitude: lon)
+    end
+
+    def default_init
+      # long lat
+      ['-70.69075584411621','-33.45382270754665']
+    end
+
+    def default_end
+      # long lat
+      ['-71.04652404785156', '-34.791074253715365']
+    end
+  end
+
+  def coords
+    [longitude, latitude]
   end
 end
