ca66a3144663baa0e9e21fc4580fecec471afd64
zetahawke <michel.szinavel@gmail.com>
2020-03-29 23:39:44 -0300

    Adding basic test if filtering endpoint

--- app/controllers/v1/road_routes_controller.rb
+++ app/controllers/v1/road_routes_controller.rb
@@ -1,7 +1,9 @@
 module V1
   class RoadRoutesController < V1::ApiController
-    def show
-      render json: { RoadRoute.filter_by(filterable_params) }
+    def index
+      render json: { road_routes: RoadRoute.filter_by(filterable_params), state: :ok }, status: :ok
+    rescue StandardError => e
+      render json: { state: :error, message: e.message }, status: :bad_request
     end
 
     private
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :v1 do
+    resources :road_routes, only: %i[index]
     resources :road_route_points, only: %i[create]
   end
 end
