029a2412405a91800f51501dfba127babd85a03a
zetahawke <michel.szinavel@gmail.com>
2020-03-30 23:55:29 -0300

    Adding filter capacity to requested endpoint

--- app/models/road_route.rb
+++ app/models/road_route.rb
@@ -7,7 +7,7 @@ class RoadRoute < ApplicationRecord
     def filter_by(params)
       destiny_coords = params[:destiny] == 'psn' ? RoadRoutePoint.default_init : RoadRoutePoint.default_end
       preload_data = params[:patent].blank? ? by_date(params[:date]) : by_date(params[:date]).by_vehicle_patent(params[:patent])
-      preload_data.when_road_to(destiny_coords)
+      presentational_map(preload_data.when_road_to(destiny_coords))
     end
 
     def when_road_to(coords = RoadRoutePoint.default_init)
@@ -21,7 +21,53 @@ class RoadRoute < ApplicationRecord
     end
 
     def by_date(date)
-      where('created_at::date == ?', date.try(:to_date))
+      where('road_routes.created_at::date = ?', date.try(:to_date))
     end
+
+    def presentational_map(data)
+      data ||= where('')
+      data.map do |road_route|
+        {
+          patent: road_route.vehicle.patent,
+          out: road_route.out,
+          arrive: road_route.arrive,
+          status: road_route.status
+        }
+      end
+    end
+  end
+
+  def out
+    return if road_route_points.blank?
+
+    road_route_points.first.created_at
+  end
+
+  def arrive
+    return if road_route_points.blank?
+
+    current_last_point = road_route_points.last
+    current_last_point.created_at if current_last_point.coords == destiny_coords
+  end
+
+  def status
+    return if road_route_points.blank?
+
+    if arrive
+      'finished'
+    else !arrive
+      current_last_point = road_route_points.last
+      if DateTime.current > current_last_point.created_at + 15.minutes
+        'incomplete'
+      else
+        'in_route'
+      end
+    end
+  end
+
+  def destiny_coords
+    return if road_route_points.blank?
+
+    road_route_points.first.coords == RoadRoutePoint.default_init ? RoadRoutePoint.default_end : RoadRoutePoint.default_end    
   end
 end
